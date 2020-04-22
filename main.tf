6b25751b5122a71016bb8a0c54b99a8e9fe2b8a1
masatomo-ito2 <45160975+masatomo-ito2@users.noreply.github.com>
2020-04-22 15:22:31 +0900

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ terraform {
   required_version = "~> 0.12"
 }
 
+/*
 provider "aws" {
   access_key = var.access_key
   secret_key = var.secret_key
@@ -20,3 +21,4 @@ resource "aws_instance" "hello-tf-instance" {
   )
 }
 
+*/
